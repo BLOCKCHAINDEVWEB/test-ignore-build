aa7721c753dc7cb4db4afbd8e62eb87de039365b
blockchaindevweb <blockchaindevweb@gmail.com>
2023-05-11 17:37:09 +0200

    test: run a test

--- script.sh
+++ script.sh
@@ -1,14 +1,3 @@
- #!/bin/bash
+#!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Proceed with the build
-    echo "✅ - Build can proceed"
-  exit 1;
-
-else
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
-fi
+if [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then exit 1; else exit 0; fi
